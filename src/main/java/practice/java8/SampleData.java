13d1a019f7c3a887763f33ba0883d415b1e5bba6
putta <putta@Mahesh>
2024-05-18 15:12:29 +0530

    Added Sample Data class

---
+++ src/main/java/practice/java8/SampleData.java
@@ -0,0 +1,17 @@
+package practice.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+// contains methods to return sample data
+public class SampleData {
+		
+	public static List<String> getNames(){	
+		return Arrays.asList("Mahesh","Kumar","Suresh","Rahul");
+	}
+	
+	public static List<Integer> getNumbers(){
+		return Arrays.asList(1,2,3,4,5,6,7,8);
+	}
+
+}
